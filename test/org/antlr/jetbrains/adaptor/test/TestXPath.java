4e7ba22a9b3e5c78cbd171cb548347015ccd836f
Terence Parr <parrt@antlr.org>
2015-12-14 09:53:51 -0800

    add initial testing rig

---
+++ test/org/antlr/jetbrains/adaptor/test/TestXPath.java
@@ -0,0 +1,10 @@
+package org.antlr.jetbrains.adaptor.test;
+
+import com.intellij.lang.ParserDefinition;
+import com.intellij.testFramework.ParsingTestCase;
+
+public class TestXPath extends ParsingTestCase {
+	public TestXPath(String dataPath, String fileExt, ParserDefinition... definitions) {
+		super(dataPath, fileExt, definitions);
+	}
+}
