48642a5396a5bc4232b9db91290d595d106c3302
Terence Parr <parrt@antlr.org>
2015-12-15 12:37:03 -0800

    rm cmt

--- adaptor/src/org/antlr/jetbrains/adaptor/psi/Trees.java
+++ adaptor/src/org/antlr/jetbrains/adaptor/psi/Trees.java
@@ -57,14 +57,6 @@ import java.util.List;
 
 public class Trees {
 	public interface Predicate<T> {
-
-		/**
-		 * Evaluates this predicate on the given argument.
-		 *
-		 * @param t the input argument
-		 * @return {@code true} if the input argument matches the predicate,
-		 * otherwise {@code false}
-		 */
 		boolean test(T t);
 	}
 
